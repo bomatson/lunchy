3fa1e88aa6028c49641ca4d717c713078781c7d7
bomatson <bobby@carbonfive.com>
2016-04-29 11:34:30 -0700

    feature flag

--- yoda/commands/nag.rb
+++ yoda/commands/nag.rb
@@ -22,9 +22,8 @@ module Yoda
   module Commands
     class Nag < SlackRubyBot::Commands::Base
       command 'nag' do |client, data, _match|
-        #emails = Timesheet.status
-        emails = ['sidney@carbonfive.com', 'jon@carbonfive.com', 'sueanna@carbonfive.com', 'ericf@carbonfive.com', 'suzanna@carbonfive.com', 'bobby@carbonfive.com']
-
+        test_emails = ['sidney@carbonfive.com', 'jon@carbonfive.com', 'sueanna@carbonfive.com', 'ericf@carbonfive.com', 'suzanna@carbonfive.com', 'bobby@carbonfive.com']
+        emails = ENV['FOR_REALZ'] ? Timesheet.status : test_emails
         users = client.users.values.select { |user| emails.include? user.profile.email }
 
         names = users.map{ |user| user.profile.first_name }
@@ -32,7 +31,7 @@ module Yoda
         client.say(channel: data.channel, text: "nagging these people about their hours: #{names.join(', ')}")
 
         users.each do |user|
-          im = client.ims.values.detect { |im| im.user == user.id }
+          im = client.ims.values.detect { |i| i.user == user.id }
           channel = if im == nil
             response = client.web_client.im_open user: user.id
             response.channel
