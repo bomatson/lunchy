043ec1fcfc3851a3601239ec824e3071421e0ddf
Mr Rogers <jon@carbonfive.com>
2016-04-29 14:35:37 -0700

    add configurable timesheet url

--- yoda/services/timesheet.rb
+++ yoda/services/timesheet.rb
@@ -1,6 +1,7 @@
 class Timesheet
   def self.status
-    url = "http://timesheet-acceptance.carbonfive.com/api/status.json?api_token=#{ENV['TIMESHEET_API_TOKEN']}"
+    timesheet_url = ENV.fetch("TIMESHEET_URL", "http://timesheet-acceptance.carbonfive.com")
+    url = "#{timesheet_url}/api/status.json?api_token=#{ENV['TIMESHEET_API_TOKEN']}"
     response = HTTParty.get(url)
     JSON.parse(response.body)['worst_offenders'].keys
   end
