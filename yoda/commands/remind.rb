ded7d982aae9446ae2a3ce744d698c1af0cfedb2
Eric Fung <ericf@carbonfive.com>
2016-04-29 11:03:36 -0700

    Stylish reminder message

--- yoda/commands/remind.rb
+++ yoda/commands/remind.rb
@@ -1,3 +1,27 @@
+class RemindHelpers
+  def self.reminder_attachment
+    {
+      "attachments": [
+        {
+          "fallback": "https://timesheet.carbonfive.com",
+          "color": "#36a64f",
+          "title": "Enter your hours in Timesheet",
+          "pretext": "#{self.random_reminder}",
+          "title_link": "https://timesheet.carbonfive.com",
+          "mrkdwn_in": ["pretext"]
+        }
+      ]
+    }
+  end
+
+  def self.random_reminder
+    [
+      "The force is strong today. Hours you will enter.",
+      "Powerful you have become. Entered hours I sense from you."
+    ].sample
+  end
+end
+
 module Yoda
   module Commands
     class Remind < SlackRubyBot::Commands::Base
@@ -6,9 +30,9 @@ module Yoda
         channels = client.channels.values + client.groups.values
         channel = channels.detect { |channel| channel.name == channel_name }
 
-        client.say(channel: channel.id, text: 'fill in your hours, you must')
+        message_options = {channel: channel.id, is_user: true}.merge(RemindHelpers.reminder_attachment)
+        client.web_client.chat_postMessage(message_options)
       end
     end
   end
 end
-
