60c96c92ddde8a0c4f15c9fe39499745e701ff92
bomatson <bobby@carbonfive.com>
2016-05-09 16:43:44 -0400

    some responses

--- lunchy/commands/add.rb
+++ lunchy/commands/add.rb
@@ -7,10 +7,11 @@ module Lunchy
         redis = Redis.new
         lunch_spot = data.text.split(/\s+/)[2..-1].join(' ')
         channel_name = data.channel
+        response = ['Great', 'Amazing', 'Nicely done', 'That place rules', 'Into it', 'Good stuff', 'Excellent']
 
         unless lunch_spot.empty?
           redis.sadd(channel_name, lunch_spot)
-          client.say(channel: channel_name, text: "Great! I've added #{lunch_spot} to the list")
+          client.say(channel: channel_name, text: "#{response.sample}! I've added #{lunch_spot} to the list")
         else
           client.say(channel: channel_name, text: "Whoops! You need to include a restaurant name, silly")
         end
