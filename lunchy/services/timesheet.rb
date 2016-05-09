2d11838c1d9c4ad924227044fbb28104903190ae
bomatson <bobby@carbonfive.com>
2016-05-09 15:27:30 -0400

    remove unused files

--- lunchy/services/timesheet.rb
+++
@@ -1,8 +0,0 @@
-class Timesheet
-  def self.status
-    timesheet_url = ENV.fetch("TIMESHEET_URL", "http://timesheet-acceptance.carbonfive.com")
-    url = "#{timesheet_url}/api/status.json?api_token=#{ENV['TIMESHEET_API_TOKEN']}"
-    response = HTTParty.get(url)
-    JSON.parse(response.body)['worst_offenders'].keys
-  end
-end
